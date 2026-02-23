ff0e91c0b61ba93d9763253e07dfb54767e3dad7
Mukesh Goud Bairu <mukesh@Mukeshs-MacBook-Air.local>
2026-02-22 19:58:17 -0500

    dependancies added

---
+++ server/index.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import express, { Express, Request, Response } from "express";
+import { MongoClient } from "mongodb";
+import cors from "cors";
+
+const app: Express = express();
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(process.env.MONGODB_ATLAS_URL as string);
+
